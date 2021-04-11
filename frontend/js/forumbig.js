12bb54082900fd26e30f9e055bfe0761e415d8fe
Diogo <dio.goffs@hotmail.com>
2021-04-12 00:16:39 +0100

    forum

---
+++ frontend/js/forumbig.js
@@ -0,0 +1,14 @@
+function failure() {
+
+    swal({
+        text: "Escolhe um",
+        buttons: {
+            cancel: "Escrever um comentário",
+            confirm: "Escolher Ficheiro",
+            hello: "Gravar Aúdio",
+        },
+    });
+
+
+
+};
