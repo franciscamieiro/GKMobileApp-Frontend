e64545da48bf8098bf3214f649b40f41f5d2a725
Francisca Mieiro <francisca.mieiro@gmail.com>
2021-04-12 01:49:01 +0100

    update

--- frontend/js/forumbig.js
+++ frontend/js/forumbig.js
@@ -7,9 +7,28 @@ function coment() {
     swal({
         text: "Escolhe um",
         buttons: {
-            cancel: "Escrever um comentário",
-            confirm: "Escolher Ficheiro",
-            hello: "Gravar Aúdio",
+            cancel:{
+                text: "Escrever um comentário",
+                value: true,
+                visible: true,
+                className: "orange",
+                closeModal: true
+            },
+            confirm:{
+                text: "Escolher ficheiro",
+                value: true,
+                visible: true,
+                className: "blue",
+                closeModal: true
+            },
+            hello:{
+                text: "Gravar aúdio",
+                value: true,
+                visible: true,
+                className: "green",
+                closeModal: true
+            },
+            
         },
     });
 
