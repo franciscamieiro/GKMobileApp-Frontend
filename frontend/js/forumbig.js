05624cc4c7ee1dd3318142d3ba8358d935d3ee63
Diogo <dio.goffs@hotmail.com>
2021-04-18 00:21:00 +0100

    a lot of updates

--- frontend/js/forumbig.js
+++ frontend/js/forumbig.js
@@ -7,31 +7,41 @@ function coment() {
     swal({
         text: "Escolhe um",
         buttons: {
-            cancel:{
+            cancel: {
                 text: "Escrever um comentário",
                 value: true,
                 visible: true,
                 className: "orange",
                 closeModal: true
             },
-            confirm:{
+            confirm: {
                 text: "Escolher ficheiro",
                 value: true,
                 visible: true,
                 className: "blue",
                 closeModal: true
             },
-            hello:{
+            hello: {
                 text: "Gravar aúdio",
                 value: true,
                 visible: true,
                 className: "green",
                 closeModal: true
             },
-            
+
         },
     });
 
 
 
 };
+
+
+function showdiv() {
+    var x = document.getElementById("browhy");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
