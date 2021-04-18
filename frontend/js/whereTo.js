a9f0d6b98ce4c2a7591a0614167e988ced34a01a
Francisca Mieiro <francisca.mieiro@gmail.com>
2021-04-18 03:46:12 +0100

    update

--- frontend/js/whereTo.js
+++ frontend/js/whereTo.js
@@ -21,29 +21,40 @@ var directionsDisplay = new google.maps.DirectionsRenderer();
 directionsDisplay.setMap(map);
 
 function calcRoute(){
-  var request = {
-     origin: document.getElementById("from").value,
-     destination: document.getElementById("to").value,
-     travelMode: google.maps.TravelMode.WALKING,
-     unitSystem: google.maps.UnitSystem.IMPERIAL
-  }
-
-  directionsService.route(request, (result, status) => {
-    if(status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(result);
-    }else{
-      directionsDisplay.setDirections({ routes: []});
+    var request = {
+      origin: document.getElementById("from").value,
+      destination: document.getElementById("to").value,
+      travelMode: google.maps.TravelMode.WALKING,
+      unitSystem: google.maps.UnitSystem.IMPERIAL
     }
-  })
-  
+
+    directionsService.route(request, (result, status) => {
+      if(status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(result);
+      }else{
+          directionsDisplay.setDirections({ routes: []});
+          swal({
+              icon: 'images/warning.png',
+              text: 'Não foram encontrados resultados!',
+          }).then(function(isConfirm) {
+              document.getElementById("from").value = "";
+              document.getElementById("to").value = "";
+          });
+        }
+    });
 }
+
 
 var options = {
   types: ['(cities)']
 }
 
 var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
 var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
+if(input1 != "" && input2 != ""){
+
+  var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+  var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
+}
