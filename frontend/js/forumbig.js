eafe2c55de0bc20c02018c474f99d868a12d4c41
Diogo <dio.goffs@hotmail.com>
2021-04-19 03:00:17 +0100

    semi fetch do forumbig

--- frontend/js/forumbig.js
+++ frontend/js/forumbig.js
@@ -244,7 +244,7 @@ function reportpub() {
             },
         },
     }).then((value) => {
-        if(value == "report"){
+        if (value == "report") {
             swal({
                 icon: 'images/v254_5.png',
                 title: 'Sucesso',
@@ -256,3 +256,36 @@ function reportpub() {
         }
     });
 };
+
+
+window.onload = () => {
+
+    const forumcreations = document.getElementById("forum");
+
+    const renderCreations = async() => {
+
+        let strHtml = ``;
+
+        const response = await fetch(`http://localhost:80/api/creations/published`)
+        const creations = await response.json()
+        let i = 1;
+        for (const creation of creations) {
+            console.log(creations);
+            strHtml += `
+            <div class="card mb-4">
+                <a href="forumbig.html" id=${creation.creationID} onclick="reply_click(this.id)" ><img src="./images/v549_72.png" class="criacoesimg img-responsive" id="popupimg"></a>
+                <div class="card-footer text-left">
+                    <img src="./images/v549_68.png" class="img-fluid v549_186" alt="">
+                    <i class="mr-1"></i> ${creation.city}
+                </div>
+            </div>
+            `;
+
+            i++;
+        }
+        forumcreations.innerHTML = strHtml;
+
+    }
+
+    renderCreations()
+}
