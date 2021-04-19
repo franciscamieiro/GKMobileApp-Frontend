0bb6a1c2cfd37a720905e86211cc0f185a552307
Diogo <dio.goffs@hotmail.com>
2021-04-19 03:16:40 +0100

    fetch forumbig worked

--- frontend/js/forum.js
+++ frontend/js/forum.js
@@ -6,25 +6,19 @@ window.onload = () => {
 
         let strHtml = ``;
 
-        const creationID = localStorage.getItem("id");
-        const response = await fetch(`http://localhost:80/api/creations/` + creationID)
+        const response = await fetch(`http://localhost:80/api/creations/published`)
         const creations = await response.json()
         let i = 1;
         for (const creation of creations) {
             console.log(creations);
             strHtml += `
-            <div class="card">
-            <div class="card-header">
-                <img src="./images/v549_186.png" class="img-fluid v549_186" alt="">
-                <i class="mr-1"></i>${creation.city}
-                <a class="mt-1 fa fa-bars fa-lg noscroll" style="float: right; color: black;" onclick="reportpub()"></a>
+            <div class="card mb-4">
+                <a href="forumbig.html" id=${creation.creationID} onclick="reply_click(this.id)" ><img src="./images/v549_72.png" class="criacoesimg img-responsive" id="popupimg"></a>
+                <div class="card-footer text-left">
+                    <img src="./images/v549_68.png" class="img-fluid v549_186" alt="">
+                    <i class="mr-1"></i> ${creation.city}
+                </div>
             </div>
-            <img src="./images/v549_72.png" class="criacoesimg img-responsive" id="popupimg">
-            <div class="card-footer">
-                <img src="./images/avatar1.png" class="img-fluid smallkidimg">
-                <i class="mr-1"></i> Autor: ${creation.user}
-            </div>
-        </div>
             `;
 
             i++;
