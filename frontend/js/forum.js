56e4be4bfcbaddfc1102847d71bc1123a9b97aea
Diogo <dio.goffs@hotmail.com>
2021-04-19 02:45:45 +0100

    fetch do forum

---
+++ frontend/js/forum.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+
+    const forumcreations = document.getElementById("forum");
+
+    const renderCreations = async() => {
+
+        let strHtml = ``;
+
+        const response = await fetch(`http://localhost:80/api/creations/published`)
+        const creations = await response.json()
+        let i = 1;
+        for (const creation of creations) {
+            console.log(creations);
+            strHtml += `
+            <div class="card mb-4">
+                <a href="forumbig.html" id=${creation.creationID} onclick="reply_click(this.id)" ><img src="./images/v549_72.png" class="criacoesimg img-responsive" id="popupimg"></a>
+                <div class="card-footer text-left">
+                    <img src="./images/v549_68.png" class="img-fluid v549_186" alt="">
+                    <i class="mr-1"></i> ${creation.city}
+                </div>
+            </div>
+            `;
+
+            i++;
+        }
+        forumcreations.innerHTML = strHtml;
+
+    }
+
+    renderCreations()
+}
