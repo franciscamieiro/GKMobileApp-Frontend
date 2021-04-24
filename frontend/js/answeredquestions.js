dd53711338886a578e25f26c49717ef0b0f36428
João Pereira <74329989+a92970@users.noreply.github.com>
2021-04-25 00:14:59 +0100

    Update answeredquestions.js

--- frontend/js/answeredquestions.js
+++ frontend/js/answeredquestions.js
@@ -5,6 +5,7 @@ function goBack() {
 window.onload = () => {
 
     const answeredQuestionsTxt = document.getElementById("doubtsText");
+    const answeredQuestionsAudio = document.getElementById("doubtsAudio");
 
     const renderAnsweredQuestionsTxt = async() => {
 
@@ -14,7 +15,7 @@ window.onload = () => {
         const doubts = await response.json()
         let i = 1;
         for (const doubt of doubts) {
-            if(!(doubt.answer == null)){
+            if((!(doubt.answer == null)) && doubt.audio == null){
                 console.log(doubts);
                 strHtml += `
                 <div class="card mb-4" id="${doubt.doubtid}">
@@ -36,5 +37,37 @@ window.onload = () => {
 
     }
 
-    renderAnsweredQuestionsTxt()
+    const renderAnsweredQuestionsAudio = async() => {
+
+        let strHtml = ``;
+
+        const response = await fetch(`http://localhost:80/api/doubts/users/`+ "3")
+        const doubts = await response.json()
+        let i = 1;
+        for (const doubt of doubts) {
+            if((!(doubt.answer == null)) && doubt.description == null){
+                console.log(doubts);
+                strHtml += `
+                <div class="card mb-4" id="${doubt.doubtid}">
+                    <div class="card-body text-center">
+                        <b class="mr-1">Dúvida</b>
+                        <hr>
+                        <audio class="playerv1" controls>
+                            <source src="images/movie_1.mp3" type="audio/mpeg">
+                        </audio>
+                        <div class="mr-1 justmargin"><b>Resposta</b></div>
+                        <hr>
+                        <div class="respostav2">${doubt.answer}</div>
+                    </div>
+                </div>
+                `;
+                i++;
+            }            
+        }
+        answeredQuestionsAudio.innerHTML = strHtml;
+
+    }
+
+    renderAnsweredQuestionsTxt();
+    renderAnsweredQuestionsAudio();
 }
