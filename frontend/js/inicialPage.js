b4019b9d7a1e249e177d84c64353fbd000c672f1
Francisca Mieiro <francisca.mieiro@gmail.com>
2021-05-16 00:38:28 +0100

    update

--- frontend/js/inicialPage.js
+++ frontend/js/inicialPage.js
@@ -3,9 +3,28 @@ let info = document.getElementById("info");
 let box = document.getElementById("box");
 let box1 = document.getElementById("box1");
 let helptxt = document.getElementById("helptxt");
-let nextBtn = document.getElementById("next");
+let close = document.getElementById("close");
 let click = 1;
 let isShowing = false;
+let orange = document.getElementById("orange");
+let yellow = document.getElementById("yellow");
+let purple = document.getElementById("purple");
+let blue = document.getElementById("blue");
+
+var viewportOffset = yellow.getBoundingClientRect();
+// these are relative to the viewport, i.e. the window
+var top = viewportOffset.top;
+var left = viewportOffset.left;
+console.log(top);
+
+box.style.display = "none";
+box1.style.marginTop = top + 20 + "px";
+box1.style.marginLeft = left - 140 + "px";
+helptxt.style.height = "18%";
+if (top >= 202) {
+    helptxt.style.height = "14%";
+    helptxt.style.marginTop = "220px";
+}
 
 logout.addEventListener("click", function () {
 
@@ -27,62 +46,61 @@ info.addEventListener("click", function () {
     if (isShowing == false) {
         box.style.display = "block";
         helptxt.style.display = "block";
-        nextBtn.style.display = "block";
         isShowing = true;
     }
 
 });
 
-function next() {
+close.addEventListener("click", function () {
 
-    if (document.body.clientWidth >= 328) {
+    if (click == 1) {
 
-        if (click == 1) {
-            box.style.display = "none";
-            box1.style.display = "block";
-            helptxt.style.height = "12%";
-            nextBtn.style.marginTop = "485px";
-            helptxt.innerHTML = "Clica no icon amarelo para ver o forúm com publicações de criações, comentar e avaliar!"
-            nextBtn.style.display = "block";
-            click++;
-        } else if (click == 2) {
-            box1.style.display = "none";
-            box.style.marginTop = "390px";
-            helptxt.style.height = "12%";
-            helptxt.style.marginTop = "250px";
-            nextBtn.style.marginTop = "200px";
-            box.style.display = "block";
-            helptxt.innerHTML = "Clica no icon roxo para desenhar novos desenhos e ver desenhos que guardaste!"
-            nextBtn.style.display = "block";
-            click++;
+        var viewportOffset = yellow.getBoundingClientRect();
+        // these are relative to the viewport, i.e. the window
+        var top = viewportOffset.top;
+        var left = viewportOffset.left;
+        console.log(top);
+
+        box.style.display = "none";
+        box1.style.marginTop = top + 20 + "px";
+        box1.style.marginLeft = left - 140 + "px";
+        helptxt.style.height = "18%";
+        if (top >= 202) {
+            helptxt.style.marginTop = "190px";
         }
-        else if (click == 3) {
-            box.style.display = "none";
-            box1.style.marginTop = "420px";
-            helptxt.style.marginTop = "250px";
-            nextBtn.style.marginTop = "200px";
-            box1.style.display = "block";
-            helptxt.style.height = "12%";
-            helptxt.innerHTML = "Clica no icon azul para ver o mapa da tua cidade, marcar locais favoritos e tirar fotos!"
-            nextBtn.innerHTML = "Fechar";
-            nextBtn.style.display = "block";
-            click++;
-        } else {
-            box.style.display = "none";
-            box1.style.display = "none";
-            helptxt.style.display = "none";
-            nextBtn.style.display = "none";
-            helptxt.innerHTML = "Clica no icon laranja para veres um caminho de um sítio para o outro!";
-            box1.style.marginTop = "220px";
-            box.style.marginTop = "190px";
-            helptxt.style.marginTop = "345px";
-            helptxt.style.height = "10%";
-            nextBtn.innerHTML = "Próxima";
-            nextBtn.style.marginTop = "472px";
-            click = 1;
-            isShowing = false;
-        }
-
+        box1.style.display = "block";
+        helptxt.innerHTML = `Clica no icon amarelo para ver o forúm com publicações de criações, comentar e avaliar!<div class="close" id="close">Próxima</div>`
+        click++;
+    } else if (click == 2) {
+        box1.style.display = "none";
+        box.style.marginTop = "390px";
+        helptxt.style.height = "12%";
+        box.style.display = "block";
+        helptxt.innerHTML = "Clica no icon roxo para desenhar novos desenhos e ver desenhos que guardaste!"
+        click++;
     }
-}
+    else if (click == 3) {
+        box.style.display = "none";
+        box1.style.marginTop = "420px";
+        helptxt.style.marginTop = "250px";
+        nextBtn.style.marginTop = "200px";
+        box1.style.display = "block";
+        helptxt.style.height = "12%";
+        helptxt.innerHTML = "Clica no icon azul para ver o mapa da tua cidade, marcar locais favoritos e tirar fotos!"
+        click++;
+    } else {
+        box.style.display = "none";
+        box1.style.display = "none";
+        helptxt.style.display = "none";
+        nextBtn.style.display = "none";
+        helptxt.innerHTML = "Clica no icon laranja para veres um caminho de um sítio para o outro!";
+        box1.style.marginTop = "220px";
+        box.style.marginTop = "190px";
+        helptxt.style.marginTop = "345px";
+        helptxt.style.height = "10%";
+        nextBtn.innerHTML = "Próxima";
+        click = 1;
+        isShowing = false;
+    }
+});
 
