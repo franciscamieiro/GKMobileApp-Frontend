6230f3c5a3923f13d2dd9bbd9f0d567c278876b9
madcientyst <dioguix@gmail.com>
2021-05-16 23:45:43 +0100

    settingiconscolor

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -12,3 +12,19 @@ function logout(){
         window.location.replace("login.html");
     });
 }
+
+let tema = localStorage.getItem('theme');
+
+function changebgimg(){
+
+    if(tema == 'dark'){
+        document.getElementById('f1').className = 'v124_205_v2';
+        document.getElementById('f2').className = 'v124_217_v2';
+        document.getElementById('f3').className = 'v124_222_v2';
+    }
+    else{
+        document.getElementById('f1').className = 'v124_205';
+        document.getElementById('f2').className = 'v124_217';
+        document.getElementById('f3').className = 'v124_222';
+    }
+}
